f54d136bcf33f664e576eaa8e3555d6672b5e655
Which-W <wengzu.love@gmail.com>
2026-01-29 13:45:47 +0800

    more hardware support push

--- GPT_LM/train_win.sh
+++ GPT_LM/train_win.sh
@@ -1,7 +1,7 @@
-python train.py \
+python3 train.py \
   --train_data_path .\data\TinyStories-train.bin \
   --valid_data_path .\data\TinyStories-valid.bin \
-  --batch_size 8 \
+  --batch_size 32 \
   --use_wandb \
   --wandb_project "tinystories-transformer" \
-  --wandb_run_name "first-experiment"
+  --wandb_run_name "fourth-experiment"
